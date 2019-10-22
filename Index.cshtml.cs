c1c24643b1b43b964fd361d03c73ef777ce64dd6
Sandra Scaffidi <54339234+slscaffidi123abc@users.noreply.github.com>
2019-10-22 12:58:49 -0500

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OneMoreTime.Data;
+
+namespace OneMoreTime.Pages.Doc
+{
+    public class IndexModel : PageModel
+    {
+        private readonly OneMoreTime.Data.ApplicationDbContext _context;
+
+        public IndexModel(OneMoreTime.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Doctors> Doctors { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Doctors = await _context.Doctors.ToListAsync();
+        }
+    }
+}
